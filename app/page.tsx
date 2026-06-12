eb0eee07ba6bfc9eba3eadae5cb1adde0a3edb44
Emre Adanır <lisa.pelet16@gmail.com>
2026-06-12 16:17:02 +0300

    Add Lisa Pelet brand name to homepage title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeContent from "../components/home/HomeContent";
 import { createPageMetadata } from "../lib/seo";
 
 export const metadata = createPageMetadata({
-  title: "Bursa Pelet | Soba Peleti ve Premium Çam Peleti",
+  title: "Lisa Pelet | Bursa Pelet, Soba Peleti ve Premium Çam Peleti",
   description:
     "Lisa Pelet: Bursa Kestel'de üretilen premium çam peleti ve soba peleti. Düşük kül, yüksek kalori. 15 kg ve 25 kg ambalaj, Bursa'ya hızlı teslimat.",
   path: "/",
